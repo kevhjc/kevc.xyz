f025d9a0e79b378f81ce5d75c385cf112d7f1625
Kevin Chung <kevin@Kevins-MacBook-Pro.local>
2022-01-24 13:50:02 -0800

    Edited component name

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 
 import '../styles/main.css'
 
-export default function Nextra({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
