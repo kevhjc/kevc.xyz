077640cf7b06a686e5234b1170d0d7a5e37684f9
Kevin Chung <kevin@Kevins-MacBook-Pro.local>
2022-01-27 14:21:13 -0800

    Removed Head component

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,8 @@
 import React from 'react'
-import Head from 'next/head'
+
 import 'nextra-theme-blog/style.css'
 import '../styles/main.css'
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title>Kevin H. Chung</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
