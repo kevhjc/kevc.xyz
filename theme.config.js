4de6b1ad239bda261e2351b16dcef2e8e8ff17f0
Kevin Chung <kevin@Kevins-MacBook-Pro.local>
2022-01-27 15:46:45 -0800

    Removed unused configs

--- theme.config.js
+++ theme.config.js
@@ -1,22 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
-const YEAR = new Date().getFullYear()
-
 export default {
-  footer: (
-    <small style={{ display: 'block', marginTop: '6rem' }}>
-      {/* <time>{YEAR}</time> © Kevin H. Chung */}
-      <style jsx>{`
-        a {
-          float: right;
-        }
-        @media screen and (max-width: 480px) {
-          article {
-            padding-top: 2rem;
-            padding-bottom: 4rem;
-          }
-        }
-      `}</style>
-    </small>
-  ),
+  footer: false,
   darkMode: true
 }
