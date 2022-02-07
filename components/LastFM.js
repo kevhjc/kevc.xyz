467559332010d8518505f35af61db50d05bbbc1d
Kevin Chung <kevin.c@me.com>
2022-02-07 10:45:24 -0800

    Update LastFM component

--- components/LastFM.js
+++ components/LastFM.js
@@ -4,12 +4,12 @@ export default function CurrentlyPlaying() {
   const lastFM = useLastFM('kevhjc', process.env.NEXT_PUBLIC_LASTFM_API_KEY)
 
   if (lastFM.status !== 'playing') {
-    return <p>Not listening to anything</p>
+    return <p>nothing</p>
   }
 
   return (
     <p>
-      Listening to <b>{lastFM.song.name}</b> by <b>{lastFM.song.artist}</b>
+      <b>{lastFM.song.name}</b> by <b>{lastFM.song.artist}</b>
     </p>
   )
 }
