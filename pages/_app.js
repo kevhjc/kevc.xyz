d1bfc7200fc788e92a9b3d95a22eddf2d4bcf900
Kevin Chung <kevin.c@me.com>
2022-02-07 20:13:43 -0800

    Add react-fade-in

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import FadeIn from 'react-fade-in'
 
 import 'nextra-theme-blog/style.css'
 import '../styles/main.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <FadeIn delay={75}>
+      <Component {...pageProps} />
+    </FadeIn>
+  )
 }
