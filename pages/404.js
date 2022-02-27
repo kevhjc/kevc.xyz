5e1a966c8c064db99cb1c565b0fbeb3734a80faf
Kevin Chung <kevin.c@me.com>
2022-02-26 16:45:05 -0800

    style: remove body padding

--- pages/404.js
+++ pages/404.js
@@ -4,11 +4,11 @@ import Link from 'next/link'
 
 export default function Custom404() {
   return (
-    <>
+    <div>
       <Head>
         <title>404 – Kevin H. Chung</title>
       </Head>
       <error>{`404 - Not found`}</error>
-    </>
+    </div>
   )
 }
