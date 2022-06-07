a7af8ed68e4f0f6e624fd2591aa53138ace76d48
Kevin Chung <kevin.c@me.com>
2022-06-06 21:20:47 -0700

    refactor: revert activity component

--- components/Activity.tsx
+++ components/Activity.tsx
@@ -28,55 +28,51 @@ export function Song() {
   }, [absoluteDate]);
 
   return (
-    <div className="text-red-500 transition-all w-fit dark:text-red-400">
-      <div className="grid items-center grid-flow-col grid-rows-3 gap-x-4">
-        <div className="relative flex-none h-20 row-span-3 overflow-hidden duration-300 ease-in-out rounded aspect-square bg-neutral-200 hover:rotate-3 hover:scale-105 dark:bg-neutral-800">
-          {cover && (
-            <Image
-              className="pointer-events-none"
-              alt={title}
-              src={cover}
-              objectFit="cover"
-              width={80}
-              height={80}
-            />
-          )}
-        </div>
-        <div className="flex items-center justify-start">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className={cn(
-              playing
-                ? 'mr-2 h-4 w-4 flex-none animate-pulse'
-                : 'mr-2 h-4 w-4 flex-none'
-            )}
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path d="M18 3a1 1 0 00-1.196-.98l-10 2A1 1 0 006 5v9.114A4.369 4.369 0 005 14c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V7.82l8-1.6v5.894A4.37 4.37 0 0015 12c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V3z" />
-          </svg>
-
-          {absoluteDate && (
-            <time
-              className="font-mono text-sm font-bold"
-              dateTime={absoluteDate.toISOString()}
-            >
-              <h2 className="font-mono text-sm font-bold">{relativeDate}</h2>
-            </time>
-          )}
-          {playing && (
-            <h2 className="font-mono text-sm font-bold">Currently listening</h2>
-          )}
-        </div>
-        <div className="font-bold truncate text-md w-fit text-neutral-800 dark:text-neutral-300">
-          {title}
-        </div>
-        <div className="text-sm font-medium truncate md:text-md w-fit text-neutral-500 dark:text-neutral-400">
-          {artist}
-          {' — '}
-          {album}
-        </div>
+    <div className="grid items-center grid-flow-col grid-rows-3 mt-8 text-red-500 transition-all w-fit gap-x-4 dark:text-red-400">
+      <div className="h-20 row-span-3 overflow-hidden duration-300 ease-in-out rounded aspect-square bg-neutral-200 hover:rotate-3 hover:scale-105 dark:bg-neutral-800">
+        {cover && (
+          <Image
+            className="pointer-events-none"
+            alt={title}
+            src={cover}
+            objectFit="cover"
+            width={200}
+            height={200}
+          />
+        )}
       </div>
+      <div className="flex items-center justify-start">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className={cn(
+            playing
+              ? 'mr-2 h-4 w-4 flex-none animate-pulse'
+              : 'mr-2 h-4 w-4 flex-none'
+          )}
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path d="M18 3a1 1 0 00-1.196-.98l-10 2A1 1 0 006 5v9.114A4.369 4.369 0 005 14c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V7.82l8-1.6v5.894A4.37 4.37 0 0015 12c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V3z" />
+        </svg>
+
+        {absoluteDate && (
+          <time
+            className="text-sm font-semibold"
+            dateTime={absoluteDate.toISOString()}
+          >
+            <h2 className="text-sm font-bold">{relativeDate}</h2>
+          </time>
+        )}
+        {playing && <h2 className="text-sm font-bold">Currently listening</h2>}
+      </div>
+      <p className="text-sm font-semibold truncate md:text-md text-neutral-800 dark:text-neutral-300">
+        {title}
+      </p>
+      <p className="text-sm font-medium truncate md:text-md text-neutral-500 dark:text-neutral-400">
+        {artist}
+        {' — '}
+        {album}
+      </p>
     </div>
   );
 }
@@ -102,7 +98,7 @@ export function Film() {
   }, [absoluteDate]);
 
   return (
-    <div className="mt-8 text-green-500 transition-all w-fit dark:text-green-400">
+    <div className="mt-8 leading-snug text-green-500 transition-all w-fit dark:text-green-400">
       <div className="grid items-center grid-flow-col grid-rows-3 gap-x-4">
         <div className="relative row-span-3 aspect-[2/3] h-20 flex-none overflow-hidden rounded bg-neutral-200 duration-300 ease-in-out hover:-rotate-3 hover:scale-105 dark:bg-neutral-800">
           {poster && (
@@ -115,7 +111,7 @@ export function Film() {
             />
           )}
         </div>
-        <div className="flex items-center justify-start w-fit">
+        <div className="flex items-center justify-start">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="flex-none w-4 h-4 mr-2"
@@ -132,19 +128,19 @@ export function Film() {
 
           {absoluteDate && (
             <time
-              className="font-mono text-sm font-bold"
+              className="text-sm font-semibold"
               dateTime={absoluteDate.toISOString()}
             >
-              <h2 className="font-mono text-sm font-bold">{relativeDate}</h2>
+              <h2 className="text-sm font-bold">{relativeDate}</h2>
             </time>
           )}
         </div>
-        <div className="font-bold truncate text-md w-fit text-neutral-800 dark:text-neutral-300">
+        <p className="text-sm font-semibold truncate md:text-md text-neutral-800 dark:text-neutral-300">
           {title}
-        </div>
-        <div className="text-sm font-medium truncate md:text-md w-fit text-neutral-500 dark:text-neutral-400">
+        </p>
+        <p className="text-sm font-medium truncate md:text-md text-neutral-500 dark:text-neutral-400">
           {year}
-        </div>
+        </p>
       </div>
     </div>
   );
@@ -154,7 +150,7 @@ export default function Activity() {
   return (
     <FadeIn delay={800} transitionDuration={800}>
       <div className="mt-16 leading-snug">
-        <h2 className="mt-10 mb-8 text-lg font-bold text-black dark:text-neutral-200">
+        <h2 className="mt-10 text-lg font-semibold text-black dark:text-neutral-200">
           Activity
         </h2>
         <Song />
