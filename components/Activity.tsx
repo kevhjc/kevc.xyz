63889c6bc8d3b0ca8d4244faba3b4f76378acc50
Kevin Chung <kevin.c@me.com>
2022-06-22 12:52:01 -0700

    fix: skeleton

--- components/Activity.tsx
+++ components/Activity.tsx
@@ -32,7 +32,7 @@ export function Song() {
     <div className="grid items-center grid-flow-col grid-rows-3 mt-6 text-red-500 transition-all w-fit gap-x-4 dark:text-red-400">
       <div className="h-20 row-span-3 overflow-hidden duration-300 ease-in-out rounded aspect-square bg-neutral-200 hover:scale-105 dark:bg-neutral-800">
         {cover && (
-          <a href={url}>
+          <a href={url} rel="noopener noreferrer" target="_blank">
             <Image
               alt={title}
               src={cover}
@@ -43,33 +43,36 @@ export function Song() {
           </a>
         )}
       </div>
-      <div className="flex items-center justify-start">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className={cn(
-            playing
-              ? 'mr-2 h-4 w-4 flex-none animate-pulse'
-              : 'mr-2 h-4 w-4 flex-none'
+      {absoluteDate || playing ? (
+        <div className="flex items-center justify-start">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={cn(
+              playing
+                ? 'mr-2 h-4 w-4 flex-none animate-pulse'
+                : 'mr-2 h-4 w-4 flex-none'
+            )}
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M18 3a1 1 0 00-1.196-.98l-10 2A1 1 0 006 5v9.114A4.369 4.369 0 005 14c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V7.82l8-1.6v5.894A4.37 4.37 0 0015 12c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V3z" />
+          </svg>
+          {absoluteDate && (
+            <h2 className="font-mono text-xs font-bold">{relativeDate}</h2>
           )}
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path d="M18 3a1 1 0 00-1.196-.98l-10 2A1 1 0 006 5v9.114A4.369 4.369 0 005 14c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V7.82l8-1.6v5.894A4.37 4.37 0 0015 12c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V3z" />
-        </svg>
-
-        {absoluteDate && (
-          <h2 className="font-mono text-xs font-bold">{relativeDate}</h2>
-        )}
-        {playing && (
-          <h2 className="font-mono text-xs font-bold">Currently listening</h2>
-        )}
-      </div>
+          {playing && (
+            <h2 className="font-mono text-xs font-bold">Currently listening</h2>
+          )}
+        </div>
+      ) : (
+        <Skeleton className="w-36" />
+      )}
       {title ? (
         <span className="font-semibold truncate text-md text-neutral-800 dark:text-neutral-200">
           {title}
         </span>
       ) : (
-        <Skeleton className="w-36" />
+        <Skeleton className="w-28" />
       )}
       {artist ? (
         <span className="text-sm font-medium truncate md:text-md text-neutral-500 dark:text-neutral-400">
