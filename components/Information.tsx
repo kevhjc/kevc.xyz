b755430955610925ddd86be4b2c47485ab321b5a
Kevin Chung <kevin.c@me.com>
2022-06-23 14:24:38 -0700

    fix: next/link, missing constant error

--- components/Information.tsx
+++ components/Information.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import FadeIn from 'react-fade-in';
 
-import { REACTRIX, CLEMENTINE, BOOKMARKR, VAVLT } from 'lib/contants';
+import { REACTRIX, CLEMENTINE, BOOKMARKR } from 'lib/contants';
 import ProjectLink from './ProjectLink';
 
 export default function Information() {
@@ -12,22 +12,22 @@ export default function Information() {
         <div className="flex">
           <div className="relative w-24 h-24 duration-300 ease-in-out hover:scale-105">
             <Link href="/">
-              <Image
-                className="rounded-full dark:invert"
-                priority={true}
-                alt="Avatar"
-                src="/avatar.png"
-                layout="fill"
-              />
+              <a>
+                <Image
+                  className="rounded-full dark:invert"
+                  priority={true}
+                  alt="Avatar"
+                  src="/avatar.png"
+                  layout="fill"
+                />
+              </a>
             </Link>
             <span className="absolute flex items-center justify-center w-8 h-8 m-auto text-center rounded-full pointer-events-none z-2 -top-1 -right-1 animate-gradient bg-gradient-to-r from-violet-400 via-emerald-400 to-rose-400">
               ✌️
             </span>
           </div>
         </div>
-        <h2 className="mt-5 text-lg font-semibold text-black dark:text-neutral-200">
-          Kevin H. Chung
-        </h2>
+        <h2 className="mt-5 text-lg font-semibold text-black dark:text-neutral-200"></h2>
         <div className="mt-3 text-neutral-800 dark:text-neutral-200">
           Software engineer based in{' '}
           <span className="font-serif text-lg italic">
