8212a182ae49f03b827294c1a015df171cbdc192
Kevin Chung <kevin.c@me.com>
2022-10-07 17:32:03 -0700

    style: update container max width

--- components/Container.tsx
+++ components/Container.tsx
@@ -4,7 +4,7 @@ export default function Container(props: ContainerProps) {
   const { children } = props;
 
   return (
-    <div className="flex flex-wrap items-center justify-center max-w-2xl p-8 mx-auto break-words w-fit md:pt-28">
+    <div className="flex flex-wrap items-center justify-center max-w-xl p-8 mx-auto break-words w-fit md:pt-28">
       <main>{children}</main>
     </div>
   );
