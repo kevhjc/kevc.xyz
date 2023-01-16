691cc2cc2a0b1fa7ac2d78d0280c92db8e4ac85e
Kevin Chung <kevin.c@me.com>
2023-01-15 22:11:55 -0800

    Remove skeleton

--- components/Music.tsx
+++ components/Music.tsx
@@ -6,8 +6,6 @@ import isYesterday from 'date-fns/isYesterday';
 import { useLatestSong } from 'hooks/useLatestSong';
 import { capitalize } from 'lib/utils';
 
-import Skeleton from './Skeleton';
-
 export default function Music() {
   const { title, artist, album, date, cover, url, playing } = useLatestSong();
 
@@ -41,46 +39,35 @@ export default function Music() {
             </a>
           )}
         </div>
-        {absoluteDate || playing ? (
-          <div className="flex items-center justify-start gap-x-2 text-red-500 dark:text-red-400">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className={cn(
-                playing
-                  ? 'h-3 w-3 flex-none animate-pulse'
-                  : 'h-3 w-3 flex-none'
-              )}
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path d="M18 3a1 1 0 00-1.196-.98l-10 2A1 1 0 006 5v9.114A4.369 4.369 0 005 14c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V7.82l8-1.6v5.894A4.37 4.37 0 0015 12c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V3z" />
-            </svg>
-            {absoluteDate && (
-              <h2 className="font-mono text-xs font-bold">{relativeDate}</h2>
+
+        <div className="flex items-center justify-start gap-x-2 text-red-500 dark:text-red-400">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={cn(
+              playing ? 'h-3 w-3 flex-none animate-pulse' : 'h-3 w-3 flex-none'
             )}
-            {playing && (
-              <h2 className="truncate font-mono text-xs font-bold">
-                Currently listening
-              </h2>
-            )}
-          </div>
-        ) : (
-          <Skeleton className="h-[1rem] w-48" />
-        )}
-        {title ? (
-          <span className="truncate text-base text-neutral-800 dark:text-neutral-200">
-            {title}
-          </span>
-        ) : (
-          <Skeleton className="h-[1rem] w-28" />
-        )}
-        {artist ? (
-          <span className="truncate text-sm text-neutral-500 dark:text-neutral-400">
-            {artist}
-          </span>
-        ) : (
-          <Skeleton className="h-[1rem] w-28" />
-        )}
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M18 3a1 1 0 00-1.196-.98l-10 2A1 1 0 006 5v9.114A4.369 4.369 0 005 14c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V7.82l8-1.6v5.894A4.37 4.37 0 0015 12c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V3z" />
+          </svg>
+          {absoluteDate && (
+            <h2 className="font-mono text-xs font-bold">{relativeDate}</h2>
+          )}
+          {playing && (
+            <h2 className="truncate font-mono text-xs font-bold">
+              Currently listening
+            </h2>
+          )}
+        </div>
+
+        <span className="truncate text-base text-neutral-800 dark:text-neutral-200">
+          {title}
+        </span>
+
+        <span className="truncate text-sm text-neutral-500 dark:text-neutral-400">
+          {artist} – {album}
+        </span>
       </div>
     </div>
   );
