b8ad4a688671dd7c9dbc5d869cd873b1eafb9623
Kevin Chung <kevin.c@me.com>
2025-04-29 17:03:12 -0700

    Adjust now playing skeleton

--- components/Container.tsx
+++ components/Container.tsx
@@ -4,7 +4,7 @@ const Container = (props: ContainerProps) => {
   const { children } = props;
 
   return (
-    <main className="m-auto min-w-0 animate-slideFadeIn sm:max-w-[520px]">
+    <main className="m-auto min-w-0 sm:max-w-[520px]">
       <div className="flex flex-1 flex-col items-center gap-y-4 p-8 sm:min-h-screen sm:justify-center">
         {children}
       </div>
